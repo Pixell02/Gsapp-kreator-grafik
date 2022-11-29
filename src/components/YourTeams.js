dac2d26d6647e5c7e1b9534c3bf5c71bdd9e5146
Pixel <nikodems1308@gmail.com>
2022-11-29 16:42:06 +0100

    add your teams page

--- src/components/YourTeams.js
+++ src/components/YourTeams.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Header from './Header';
+import LeftBar from './Left-Bar';
+import MainYourTeams from './main-content-elements/MainYourTeams';
 
+import "../App.css";
 function YourTeams () {
 
     return (
-        <div>
-
-        </div>
+        <div className="page-container">
+      <div className='content-wrap'>
+        <Header />
+        <LeftBar />
+        <MainYourTeams />
+      </div>
+    </div>
     );
 }
 
