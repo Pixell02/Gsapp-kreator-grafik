3ec4d0dbdb394cb901e85295e1e8dd53510a449a
Pixel <nikodems1308@gmail.com>
2022-11-29 16:41:33 +0100

    add opponent page

--- src/components/Opponents.js
+++ src/components/Opponents.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Header from './Header';
+import LeftBar from './Left-Bar';
+import OpponentsMainContent from './main-content-elements/Opponents-MainContent';
+
+import "../App.css";
 
 function Opponents () {
 
     return (
-        <div>
-
+      <div className="page-container">
+        <div className='content-wrap'>
+          <Header />
+          <LeftBar />
+          <OpponentsMainContent />        
         </div>
+      </div>
     );
 }
 
