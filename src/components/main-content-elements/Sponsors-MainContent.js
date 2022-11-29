6d06ec721d3b69c8d22632b306b60ad60e26dff2
Pixel <nikodems1308@gmail.com>
2022-11-29 16:45:19 +0100

    sponsor content add

---
+++ src/components/main-content-elements/Sponsors-MainContent.js
@@ -0,0 +1,21 @@
+import React from "react";
+import MainFooter from "../MainFooter";
+import Title from "./Title";
+import Buttons from "./Buttons";
+import ItemContainer from "./ItemContainer";
+import Block from "./Block";
+
+function SponsorsMainContent() {
+    return (
+        <div className="main-content">
+          <div className="ml-5">
+            <Title title = "Sponsorzy" />
+            <Buttons name = "Dodaj sponsora"/>
+            <ItemContainer element = {<Block />} />
+          </div>
+          <MainFooter />
+        </div>
+    );
+}
+
+export default SponsorsMainContent;
