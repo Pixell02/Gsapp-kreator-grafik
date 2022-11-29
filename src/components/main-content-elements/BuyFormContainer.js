c49a8a05bed9566a11f5d96eb0719b1efb06a093
Pixel <nikodems1308@gmail.com>
2022-11-29 16:42:34 +0100

    buy form add

---
+++ src/components/main-content-elements/BuyFormContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function BuyFormContainer() {
+
+    return (
+        <div className='form-container'>
+            <div className='logo-container'>
+                <span className='logo'>LOGO</span>
+            </div>
+            <div className='price-container'>
+              <h1>50zł</h1>
+            </div>
+            <div className='form'>
+                <input type='text' className='name' placeholder='Imię' />
+                <input type='text' className='surName' placeholder='Nazwisko' />
+                <input type='text' className='adress' placeholder='Adres' />
+                <input type='text' className='post-code' placeholder='Kod pocztowy' />
+                <input type='text' className='city' placeholder='Miasto' />
+                <button className='buy-btn btn btn-primary'>Kup</button>
+            </div>
+        </div>
+    );
+}
+
+export default BuyFormContainer;
