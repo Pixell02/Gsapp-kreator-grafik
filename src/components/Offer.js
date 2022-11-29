df549e161df0689fa9443721ffb84931636beb9c
Pixel <nikodems1308@gmail.com>
2022-11-29 16:41:20 +0100

    add mainContentOffer

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Header from './Header';
+import LeftBar from './Left-Bar';
+import MainContentOffer from './main-content-elements/MainContentOffer';
+import "../App.css";
 
 function Offer () {
 
     return (
-        <div>
-
+      <div className="page-container">
+        <div className='content-wrap'>
+          <Header />
+          <LeftBar />
+          <MainContentOffer />
         </div>
+      </div>  
     );
 }
 
