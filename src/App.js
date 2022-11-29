31d924e649a74d1cf0337946579f1817d8e09d52
Pixel <nikodems1308@gmail.com>
2022-11-29 01:47:18 +0100

    route add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import FormPage from "./components/FormPage";
+import {BrowserRouter,Link ,Route, Routes} from "react-router-dom";
 import { authentication } from "./firebase-config";
-import './App.css';
-
+import Header from "./components/Header";
+import Footer from "./components/MainFooter";
+import FormPage from "./components/form-elements/FormPage";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Generator from "./components/Generator";
+import Catalog from "./components/Catalog";
+import YourTeamPanel from "./components/YourTeamPanel";
+import Players from "./components/Players";
+import Opponents from "./components/Opponents";
+import Sponsors from "./components/Sponsors";
+import YourTeams from "./components/YourTeams";
+import Offer from "./components/Offer";
+import Account from "./components/Account";
+import "./App.css"
 function App() {
 
   return (
-  <div className="page-container">
-    <div className="content-wrap">
-      <Header />
-      <div className="form-align-center">
-        <FormPage />
-      </div>
-    </div>
-      
-  </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element = {<Login/>} />
+      <Route path="/register" element = {<Register />} />
+      <Route path="/generator" element = {<Generator />} />
+      <Route path="/catalog" element = {<Catalog />} />
+      <Route path="/yourTeamPanel" element = {<YourTeamPanel />} />
+      <Route path="/players" element = {<Players />} />
+      <Route path="/opponents" element = {<Opponents />} />
+      <Route path="/sponsors" element = {<Sponsors />} />
+      <Route path="/yourTeams" element = {<YourTeams />} />
+      <Route path="/offer" element = {<Offer />} />
+      <Route path="/account" element = {<Account />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
