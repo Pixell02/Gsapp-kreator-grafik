eddddce6718692b0432d775fb604d0ce18ca4346
Pixel <nikodems1308@gmail.com>
2022-12-03 02:28:30 +0100

    add import addposteringenerator

--- src/components/main-content-elements/MainGenerator.js
+++ src/components/main-content-elements/MainGenerator.js
@@ -2,16 +2,15 @@ import React from 'react';
 import Title from './Title';
 import Footer from '../MainFooter';
 import ItemContainer from './ItemContainer';
-import Catalog from './Block';
+import AddPosterInGenerator from './addPosterInGenerator';
 import "./Main.css";
-import plusIcon from '../../img/plusIcon.png';
 function MainGenerator() {
 
     return (
         <div className='main-content'>
             <div className="ml-5">
             <Title  title = "Wygeneruj grafikę"/>
-            {/* <Catalog key= "0"  title = "wybierz grafikę" img = {plusIcon} /> */}
+            <AddPosterInGenerator />
             <Title title = "Ulubione" />
             <ItemContainer />
             <Title title = "Propozycje" />
