06d0c9a4441fc8d2b110c8fccae50b1662209ffe
Pixel <nikodems1308@gmail.com>
2022-12-03 23:30:26 +0100

    something add

--- src/pages/YourTeams/YourTeams.js
+++ src/pages/YourTeams/YourTeams.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import {useState} from 'react';
 import Header from '../../components/Header';
 import LeftBar from '../../components/Left-Bar';
 import MainYourTeams from '../../components/main-content-elements/MainYourTeams';
+import AddTeamWindow from "../../components/main-content-elements/addTeamWindow";
 import "../../App.css";
+
 function YourTeams () {
+  const [openModal, setOpenModal] = useState(true)
 
     return (
         <div className="page-container">
+          <AddTeamWindow open={setOpenModal} />
       <div className='content-wrap'>
         <Header />
         <LeftBar />
