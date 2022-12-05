4031bcad72f40834c03ae658e43c70b31c783d2a
Pixel <nikodems1308@gmail.com>
2022-12-05 20:11:45 +0100

    delete import make log in by facebook and google

--- src/components/form-elements/FormPage.js
+++ src/components/form-elements/FormPage.js
@@ -1,102 +1,111 @@
-import { Navigate, useNavigate} from "react-router-dom";
-import { useState } from 'react'; 
-import { useLogin } from '../../hooks/useLogin'
-import { signInWithPopup, FacebookAuthProvider, GoogleAuthProvider} from "firebase/auth";
-import { auth } from "../../firebase/config"
+import { useNavigate } from "react-router-dom";
+import { useState, useRouter } from "react";
+import { useLogin } from "../../hooks/useLogin";
+import { useAuthContext } from '../../hooks/useAuthContext';
+
+import {
+  signInWithPopup,
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "../../firebase/config";
+import { UserAuth } from "../../context/AuthContext";
+
 // import styles and images
-import './formPage.css';
-import google from '../../img/google.png';
-import facebook from '../../img/fb.png';
+import "./formPage.css";
+import google from "../../img/google.png";
+import facebook from "../../img/fb.png";
+import { FirebaseError } from "firebase/app";
 
-function LoginPage (props) {
-    
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const { error, login } = useLogin();
+function LoginPage(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { error, login } = useLogin();
+  const [user, setUser] = useState({});
+  const { dispatch } = useAuthContext()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      login(email, password);
-      
+  const signInWithFacebook = async() => {
+    const provider = new FacebookAuthProvider();
+    await signInWithPopup(auth, provider)
+      try {
+        await signInWithPopup(auth, provider)
+        .then((res) => {
+          dispatch({type: 'LOGIN', payload: res.user});
+          navigate('/YourTeams');
+        })
+      } catch (err) {
+        console.log(err);
+      }
+  };
+
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth,provider)
+      .then((res) => {
+        dispatch({type: 'LOGIN', payload: res.user })
+        navigate('/YourTeams');
+      })
+    } catch (err) {
+      console.log(err);
     }
-    const navigate = useNavigate();
-
-    const signInWithFacebook = () => {
-      
-      const provider = new FacebookAuthProvider();
-      signInWithPopup(auth, provider)
-       .then((res) => {
-          // navigate('/uid/generator')
-       })
-       .catch((err) => {
-        console.log(err.message);
-       })
-       
-       
-    }
-    const signInWithGoogle = () => {
-      const provider = new GoogleAuthProvider();
-      signInWithPopup(auth, provider)
-       .then((res) => {
-        // navigate('/generator')
-       })
-       .catch((err) =>
-       console.log(err)
-       )
-       
-    }
-
-    return (
-        <form onSubmit={handleSubmit} className="form">
-          <div className="form-group">
-            <div className="text-left">
-                <p className='login'>{props.name}</p>
+  };
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <div className="form-group">
+        <div className="text-left">
+          <p className="login">{props.name}</p>
+        </div>
+        <div className="google-btn">
+          <button onClick={ signInWithGoogle }>
+            <div className="logo-container">
+              <img src={google} alt="google_logo" className="logo" />
             </div>
-            <div className="google-btn">
-              <button onClick={ signInWithGoogle }>
-                <div className='logo-container'>
-                  <img src={google} alt="google_logo" className='logo'/>
-                </div>
-                  <div className='login-content'>
-                    <span> {props.name} przy pomocy Google</span>
-                  </div>
-              </button>
+            <div className="login-content">
+              <span> {props.name} przy pomocy Google</span>
             </div>
-            <div className="facebook-btn">
-                <button onClick={signInWithFacebook} >
-                  <div className="logo-container">
-                    <img src={facebook} alt="facebook_logo" className='logo' />
-                  </div>
-                    <div className="login-content">
-                      <span> {props.name} przy pomocy facebooka</span>
-                    </div>
-                </button>
+          </button>
+        </div>
+        <div className="facebook-btn">
+          <button onClick={() => signInWithFacebook()}>
+            <div className="logo-container">
+              <img src={facebook} alt="facebook_logo" className="logo" />
             </div>
-            <div className="email-container">
-              <input type="email"
-               name="email"
-               placeholder='email *'
-               onChange={(e) => setEmail(e.target.value)}
-               value={email}
-               />
+            <div className="login-content">
+              <span> {props.name} przy pomocy facebooka</span>
             </div>
-            <input type="password" 
-            name="password" 
-            placeholder='hasło *'
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            />
-            <div className="email-container">
-              <button className='btn btn-dark button' >Zaloguj się</button>
-              
-            </div>
-            {error && <p>{error}</p>}
-            <div className="text-left register-container">
-             {props.footer}
-            </div>
-          </div>
-         </form>
-    );
+          </button>
+        </div>
+        <div className="email-container">
+          <input
+            type="email"
+            name="email"
+            placeholder="email *"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+        <input
+          type="password"
+          name="password"
+          placeholder="hasło *"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <div className="email-container">
+          <button className="btn btn-dark button">Zaloguj się</button>
+        </div>
+        {error && <p>{error}</p>}
+        <div className="text-left register-container">{props.footer}</div>
+      </div>
+    </form>
+  );
 }
 
 export default LoginPage;
