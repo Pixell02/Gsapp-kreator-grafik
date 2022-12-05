2191cd46eaeb1a52a86b8f6affef514b082fa4eb
Pixel <nikodems1308@gmail.com>
2022-12-05 20:07:40 +0100

    delete states

--- src/pages/YourTeams/YourTeams.js
+++ src/pages/YourTeams/YourTeams.js
@@ -6,11 +6,9 @@ import AddTeamWindow from "../../components/main-content-elements/addTeamWindow"
 import "../../App.css";
 
 function YourTeams () {
-  const [openModal, setOpenModal] = useState(true)
 
     return (
         <div className="page-container">
-          <AddTeamWindow open={setOpenModal} />
       <div className='content-wrap'>
         <Header />
         <LeftBar />
