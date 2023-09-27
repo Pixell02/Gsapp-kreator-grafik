2f447eeff88fbfd923823afa88adcec0250e27aa
Pixel <nikodems1308@gmail.com>
2023-09-27 16:31:57 +0200

    format code with auto formatter

--- src/pages/Creator/components2/EditPanelComponent/SingleElements/TeamOption.js
+++ src/pages/Creator/components2/EditPanelComponent/SingleElements/TeamOption.js
@@ -1,54 +1,53 @@
-import React, { useEffect } from 'react'
-import { YourTeamNameAndLogo } from '../../../hooks2/useYourTeamLogo';
-import Select from 'react-select';
-import { useCollection } from '../../../../../hooks/useCollection';
-import { useAuthContext } from '../../../../../hooks/useAuthContext';
-import teamLogo from './TeamOption/teamLogo';
-import { useContext } from 'react';
-import radioContext from '../../../context/radioContext';
-import teamFullName from './TeamOption/teamFullName';
-import teamFirstName from './TeamOption/teamFirstName';
-import teamSecondName from './TeamOption/teamSecondName';
-import translate from "../../../locales/translate.json"
+import React, { useContext, useEffect } from "react";
+import Select from "react-select";
 import { LanguageContext } from "../../../../../context/LanguageContext";
+import radioContext from "../../../context/radioContext";
+import { YourTeamNameAndLogo } from "../../../hooks2/useYourTeamLogo";
+import translate from "../../../locales/translate.json";
+import teamFirstName from "./TeamOption/teamFirstName";
+import teamFullName from "./TeamOption/teamFullName";
+import teamLogo from "./TeamOption/teamLogo";
+import teamSecondName from "./TeamOption/teamSecondName";
 
+export default function TeamOption({
+  fabricRef,
+  coords,
+  themeOption,
+  posterBackground,
+}) {
+  const { language } = useContext(LanguageContext);
 
-export default function TeamOption({ fabricRef, coords, themeOption, posterBackground }) {
-  const {language} = useContext(LanguageContext)
-  const { user } = useAuthContext();
-  const { documents: Logo } = useCollection("Teams", ["uid", "==", user.uid]);
-  const { radioChecked } = useContext(radioContext)
+  const { radioChecked } = useContext(radioContext);
 
-  const [yourTeam, teamOption, getTeamOption, yourLogo, yourName] = YourTeamNameAndLogo(Logo);
-  
+  const [yourTeam, teamOption, getTeamOption, yourLogo, yourName] =
+    YourTeamNameAndLogo();
+
   useEffect(() => {
     if (fabricRef.current?._objects && yourLogo) {
-      teamLogo(fabricRef, yourLogo, coords, themeOption, radioChecked)
+      teamLogo(fabricRef, yourLogo, coords, themeOption, radioChecked);
     }
-  }, [fabricRef.current, yourLogo, radioChecked])
+  }, [fabricRef.current, yourLogo, radioChecked]);
   useEffect(() => {
-    
     if (fabricRef.current?._objects && yourName) {
       if (coords?.yourTeamName) {
-        teamFullName(fabricRef, yourName, coords, themeOption, radioChecked)
-      } 
+        teamFullName(fabricRef, yourName, coords, themeOption, radioChecked);
+      }
       if (coords?.yourTeamFirstName && yourName) {
-        teamFirstName(fabricRef, yourName, coords, themeOption, radioChecked)
+        teamFirstName(fabricRef, yourName, coords, themeOption, radioChecked);
       }
       if (coords?.yourTeamSecondName && yourName) {
-        teamSecondName(fabricRef, yourName, coords, themeOption, radioChecked)
-       }
+        teamSecondName(fabricRef, yourName, coords, themeOption, radioChecked);
+      }
     }
-  }, [fabricRef.current,yourName, radioChecked, posterBackground])
-  
+  }, [fabricRef.current, yourName, radioChecked, posterBackground]);
   return (
     <div>
       {teamOption && teamOption.length > 1 && (
-                <>
+        <>
           <label>{translate.yourTeam[language]}</label>
-                  <Select options={teamOption} onChange={getTeamOption} />
-                </>
-              )}
+          <Select options={teamOption} onChange={getTeamOption} />
+        </>
+      )}
     </div>
-  )
+  );
 }
