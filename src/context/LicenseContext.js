c626bdbbe65db8203af1eaab22e26e641a90494c
Pixel <nikodems1308@gmail.com>
2023-09-27 12:37:44 +0200

    create License context file, getting license information from firestore database

--- src/context/LicenseContext.js
+++ src/context/LicenseContext.js
@@ -1,20 +1,33 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { createContext } from "react";
-
-
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { createContext, useEffect, useState } from "react";
+import { db } from "../firebase/config";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export const LicenseContext = createContext();
 
-
 export const LicenseProvider = ({ children }) => {
-  const [license, setLicense] = useState();
+  const [license, setLicense] = useState(null);
+  const { user } = useAuthContext();
+  useEffect(() => {
+    if (user) {
+      let ref = collection(db, "user");
+      ref = query(ref, where("uid", "==", user.uid));
 
-  
+      const unsub = onSnapshot(ref, (snapshot) => {
+        let results = [];
+        snapshot.docs.forEach((doc) => {
+          results.push({ ...doc.data(), id: doc.id });
+        });
+
+        setLicense(results[0]);
+      });
+      return () => unsub();
+    }
+  }, [user]);
+
   return (
     <LicenseContext.Provider value={{ license, setLicense }}>
       {children}
     </LicenseContext.Provider>
-  )
-
-}
+  );
+};
