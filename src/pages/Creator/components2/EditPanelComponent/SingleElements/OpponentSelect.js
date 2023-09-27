7e985768db5093aaacbd2e4abbde96820a674a5a
Pixel <nikodems1308@gmail.com>
2023-09-27 16:10:49 +0200

    format code with auto formatter

--- src/pages/Creator/components2/EditPanelComponent/SingleElements/OpponentSelect.js
+++ src/pages/Creator/components2/EditPanelComponent/SingleElements/OpponentSelect.js
@@ -1,34 +1,54 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Select from "react-select";
-import opponentLogo from "./TeamOption/opponentLogo";
-import { useContext } from "react";
-import radioContext from "../../../context/radioContext";
-import opponentsFullName from "./TeamOption/opponentsFullName";
-import opponentsFirstName from "./TeamOption/opponentsFirstName";
-import opponentsSecondName from "./TeamOption/opponentsSecondName";
-import translate from "../../../locales/translate.json"
 import { LanguageContext } from "../../../../../context/LanguageContext";
+import { useAuthContext } from "../../../../../hooks/useAuthContext";
+import { useCollection } from "../../../../../hooks/useCollection";
+import useTeamLicenseCollection from "../../../../../hooks/useTeamLicenseCollection";
+import radioContext from "../../../context/radioContext";
+import translate from "../../../locales/translate.json";
+import opponentLogo from "./TeamOption/opponentLogo";
+import opponentsFirstName from "./TeamOption/opponentsFirstName";
+import opponentsFullName from "./TeamOption/opponentsFullName";
+import opponentsSecondName from "./TeamOption/opponentsSecondName";
 
-const OpponentSelect = ({ Opponents, fabricRef, coords, posterBackground, themeOption }) => {
+const OpponentSelect = ({
+  fabricRef,
+  coords,
+  posterBackground,
+  themeOption,
+}) => {
+  const { user } = useAuthContext();
+  const { documents: LicenseOpponents } = useTeamLicenseCollection("Opponents");
   const [opponentsName, setOpponentsName] = useState("");
   const [opponentsLogo, setOpponentsLogo] = useState("");
   const [options, setOptions] = useState([]);
   const { radioChecked } = useContext(radioContext);
-  const {language} = useContext(LanguageContext)
- 
-
+  const { language } = useContext(LanguageContext);
+  const { documents: Opponents } = useCollection("Opponents", [
+    "uid",
+    "==",
+    user.uid,
+  ]);
   useEffect(() => {
-    if (Opponents) {
+    if (Array.isArray(Opponents)) {
+      // Check if both are arrays
       const options = Opponents.map((opponent) => ({
         value: opponent.img,
         label: `${opponent.firstName} ${opponent.secondName}`,
       }));
-
-      setOptions(options);
+      setOptions([...options]);
     }
-  }, [Opponents]);
+    if (Array.isArray(LicenseOpponents)) {
+      const additionalOption = LicenseOpponents.map((item) => ({
+        value: item.img,
+        label: `${item.firstName} ${item.secondName}`,
+      }));
+
+      setOptions([...additionalOption]);
+    }
+
+    // Concatenate the two arrays and update the state
+  }, [Opponents, LicenseOpponents]);
 
   const setOpponent = (option) => {
     fetch(`${option.value}`)
@@ -44,29 +64,60 @@ const OpponentSelect = ({ Opponents, fabricRef, coords, posterBackground, themeO
   };
   useEffect(() => {
     if (fabricRef.current?._objects && opponentsLogo) {
-      
       opponentLogo(fabricRef, opponentsLogo, coords, themeOption, radioChecked);
     }
-  }, [fabricRef.current, coords, posterBackground, themeOption, opponentsLogo, radioChecked]);
+  }, [
+    fabricRef.current,
+    coords,
+    posterBackground,
+    themeOption,
+    opponentsLogo,
+    radioChecked,
+  ]);
 
   useEffect(() => {
     if (fabricRef.current?._objects && opponentsName !== "") {
       if (coords.opponentFirstName) {
-        opponentsFirstName(fabricRef, opponentsName, coords, themeOption, radioChecked);
+        opponentsFirstName(
+          fabricRef,
+          opponentsName,
+          coords,
+          themeOption,
+          radioChecked
+        );
       }
       if (coords.opponentSecondName) {
-        opponentsSecondName(fabricRef, opponentsName, coords, themeOption, radioChecked);
+        opponentsSecondName(
+          fabricRef,
+          opponentsName,
+          coords,
+          themeOption,
+          radioChecked
+        );
       }
       if (coords.opponentName) {
-        opponentsFullName(fabricRef, opponentsName, coords, themeOption, radioChecked);
+        opponentsFullName(
+          fabricRef,
+          opponentsName,
+          coords,
+          themeOption,
+          radioChecked
+        );
       }
     }
-  }, [fabricRef.current, coords, posterBackground, themeOption, opponentsName, radioChecked]);
+  }, [
+    fabricRef.current,
+    coords,
+    posterBackground,
+    themeOption,
+    opponentsName,
+    radioChecked,
+  ]);
 
   return (
     <>
       <label>{translate.Opponents[language]}</label>
-      {Opponents && <Select options={options} onChange={setOpponent} /> }
+      {Opponents && <Select options={options} onChange={setOpponent} />}
     </>
   );
 };
