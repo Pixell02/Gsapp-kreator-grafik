cc81172de58238053839d8fa724b7e5a2056e7ac
Pixel <nikodems1308@gmail.com>
2023-09-27 13:05:09 +0200

    save file with auto formatter

--- src/pages/Creator/components2/EditPanelComponent/SelectWindow/components/MultiElements/hooks/useResults.js
+++ src/pages/Creator/components2/EditPanelComponent/SelectWindow/components/MultiElements/hooks/useResults.js
@@ -1,21 +1,34 @@
-import React, { useState } from 'react'
-import useAddMultiplyImageAndText from './useAddMultiplyImageAndText';
-import { useEffect } from 'react';
-import useProperties from './useProperties';
+import { useEffect, useState } from "react";
+import useAddMultiplyImageAndText from "./useAddMultiplyImageAndText";
+import useProperties from "./useProperties";
 
 const useResults = (fabricRef, coords, selectedMatch) => {
   const { properties } = useProperties(coords);
-  const [hostResult, setHostResult] = useState('');
-  const [guestResult, setGuestResult] = useState('');
-  const { handleAddText } = useAddMultiplyImageAndText(fabricRef, selectedMatch);
-  console.log(coords)
+  const [hostResult, setHostResult] = useState("");
+  const [guestResult, setGuestResult] = useState("");
+  const { handleAddText } = useAddMultiplyImageAndText(
+    fabricRef,
+    selectedMatch
+  );
+
   useEffect(() => {
-    if (coords.connectedResultsOne && (hostResult || guestResult)) handleAddText(coords.connectedResultsOne, (hostResult || "") + " " + (coords.connectedResultsOne.Formatter || ":") + " " + (guestResult || ""), properties)
-    if (coords.yourTeamResultOne && hostResult) handleAddText(coords.yourTeamResultOne, hostResult, properties);
-    if (coords.opponentTeamResultOne && guestResult) handleAddText(coords.opponentTeamResultOne, guestResult, properties);
-  },[hostResult, guestResult])
+    if (coords.connectedResultsOne && (hostResult || guestResult))
+      handleAddText(
+        coords.connectedResultsOne,
+        (hostResult || "") +
+          " " +
+          (coords.connectedResultsOne.Formatter || ":") +
+          " " +
+          (guestResult || ""),
+        properties
+      );
+    if (coords.yourTeamResultOne && hostResult)
+      handleAddText(coords.yourTeamResultOne, hostResult, properties);
+    if (coords.opponentTeamResultOne && guestResult)
+      handleAddText(coords.opponentTeamResultOne, guestResult, properties);
+  }, [hostResult, guestResult]);
 
-  return {hostResult ,setHostResult, guestResult, setGuestResult}
-}
+  return { hostResult, setHostResult, guestResult, setGuestResult };
+};
 
-export default useResults
+export default useResults;
