db1b8b0b69e59bd54ba84b3e7e6d436bc8c68d98
Pixel <nikodems1308@gmail.com>
2023-09-27 18:12:33 +0200

    create component to read data for user and main account

---
+++ src/pages/YourTeamPanel/components/YourTeamContainer.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import EditYourTeamWindow from "./EditYourTeamWindow";
+import "./YourTeamBlock.css";
+
+import useTeamLicenseCollection from "../../../hooks/useTeamLicenseCollection";
+import TeamBlock from "./TeamBlock";
+
+export default function YourTeamContainer({ Team }) {
+  const [openEditYourTeam, setOpenEditYourTeam] = useState(false);
+  const { documents: licenseTeams } = useTeamLicenseCollection("Teams");
+  const [data, setData] = useState(null);
+  const editClick = (e, team) => {
+    setOpenEditYourTeam(true);
+    setData(team);
+  };
+
+  return (
+    <div className="your-team-catalog-container d-flex flex-wrap">
+      {Team?.map((team) => (
+        <TeamBlock team={team} editClick={editClick} />
+      ))}
+      {licenseTeams?.map((item, i) => (
+        <TeamBlock team={item} editClick={editClick} />
+      ))}
+      {data && openEditYourTeam && (
+        <EditYourTeamWindow
+          yourTeam={data}
+          open={openEditYourTeam}
+          onClose={(e) => setOpenEditYourTeam(false)}
+        />
+      )}
+    </div>
+  );
+}
