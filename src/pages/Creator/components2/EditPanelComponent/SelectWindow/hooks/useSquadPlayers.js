e0ab123e6396ca95de1368033e37a47ccba181c9
Pixel <nikodems1308@gmail.com>
2023-09-27 15:27:18 +0200

    add reading players from main account

--- src/pages/Creator/components2/EditPanelComponent/SelectWindow/hooks/useSquadPlayers.js
+++ src/pages/Creator/components2/EditPanelComponent/SelectWindow/hooks/useSquadPlayers.js
@@ -1,15 +1,32 @@
-import React, { useContext, useState } from 'react'
-import { useAuthContext } from '../../../../../../hooks/useAuthContext';
-import { useCollection } from '../../../../../../hooks/useCollection';
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../../../../../../hooks/useAuthContext";
+import { useCollection } from "../../../../../../hooks/useCollection";
+import useTeamLicenseCollection from "../../../../../../hooks/useTeamLicenseCollection";
 
 const useSquadPlayers = () => {
   const { user } = useAuthContext();
-  const { documents: Players } = useCollection("Players", ["uid", "==", user.uid]);
+  const [Players, setPlayers] = useState(null);
+  const { documents: TeamPlayers } = useCollection("Players", [
+    "uid",
+    "==",
+    user.uid,
+  ]);
+  const { documents: LicensedPlayers } = useTeamLicenseCollection("Players");
   const [selectedPlayers, setSelectedPlayers] = useState([]);
   const [selectedReserve, setSelectedReserve] = useState([]);
+
+  useEffect(() => {
+    if (TeamPlayers?.length > 0) {
+      setPlayers([...TeamPlayers]);
+    }
+    if (LicensedPlayers?.length > 0) {
+      setPlayers([...LicensedPlayers]);
+    }
+  }, [TeamPlayers, LicensedPlayers]);
+
   const handlePlayerChecked = (player) => {
     const { firstName, secondName, number, age } = player;
-    
+
     const isSelected = selectedPlayers.some(
       (selectedPlayer) =>
         selectedPlayer.firstName === firstName &&
@@ -23,23 +40,22 @@ const useSquadPlayers = () => {
           (selectedPlayer) =>
             selectedPlayer.firstName !== firstName ||
             selectedPlayer.secondName !== secondName ||
-            selectedPlayer.number !== number 
+            selectedPlayer.number !== number
         )
       );
     } else {
-if(selectedPlayers.length !== 11){
-      setSelectedPlayers((prevSelectedPlayers) => [
-        ...prevSelectedPlayers,
-        { firstName, secondName, number, age },
-      ]);
-      }else {
-  }
+      if (selectedPlayers.length !== 11) {
+        setSelectedPlayers((prevSelectedPlayers) => [
+          ...prevSelectedPlayers,
+          { firstName, secondName, number, age },
+        ]);
+      } else {
+      }
     }
-   
   };
   const handleReserveChecked = (reserve) => {
     const { firstName, secondName, number, age } = reserve;
-    
+
     const isSelected = selectedReserve.some(
       (selectedReserve) =>
         selectedReserve.firstName === firstName &&
@@ -57,20 +73,23 @@ if(selectedPlayers.length !== 11){
         )
       );
     } else {
-if(selectedReserve.length !== 9){
-      setSelectedReserve((prevselectedReserves) => [
-        ...prevselectedReserves,
-        { firstName, secondName, number, age },
-      ]);
+      if (selectedReserve.length !== 9) {
+        setSelectedReserve((prevselectedReserves) => [
+          ...prevselectedReserves,
+          { firstName, secondName, number, age },
+        ]);
       } else {
-    
-  }
+      }
     }
-   
-  }
+  };
 
+  return {
+    Players,
+    selectedPlayers,
+    handlePlayerChecked,
+    handleReserveChecked,
+    selectedReserve,
+  };
+};
 
-  return {Players, selectedPlayers, handlePlayerChecked, handleReserveChecked, selectedReserve}
-}
-
-export default useSquadPlayers
+export default useSquadPlayers;
