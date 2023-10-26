019fb610094e43b0ebef1fa8ae5e4168ba26f98a
Pixel <nikodems1308@gmail.com>
2023-10-26 21:57:29 +0200

    added opacity

--- src/pages/Creator/hooks/useIsActive.js
+++ src/pages/Creator/hooks/useIsActive.js
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from "react";
 
 const useIsActive = (fabricRef) => {
   const [isActive, setIsActive] = useState(false);
 
   const handleActiveObject = (item) => {
     fabricRef.current.setActiveObject(item);
-    setIsActive(true)
+    item.opacity = 0.2;
+    setIsActive(true);
     fabricRef.current.renderAll();
-  }
+  };
 
-  
-  const handleDeActiveObject = () => {
-      fabricRef.current.discardActiveObject();
-      fabricRef.current.renderAll();
-      setIsActive(false);
+  const handleDeActiveObject = (item) => {
+    fabricRef.current.discardActiveObject();
+    item.opacity = 1;
+    fabricRef.current.renderAll();
+    setIsActive(false);
   };
 
   return { isActive, handleDeActiveObject, handleActiveObject };
--- src/pages/Creator/hooks/useTeamOption.js
+++ src/pages/Creator/hooks/useTeamOption.js
@@ -4,26 +4,30 @@ import { useCollection } from "../../../hooks/useCollection";
 import useTeamLicenseCollection from "../../../hooks/useTeamLicenseCollection";
 
 const useTeamOption = () => {
-  const [teamOption, setTeamOption] = useState();
+  const [teamOption, setTeamOption] = useState([]);
   const { user } = useAuthContext();
   const { documents: Logo } = useCollection("Teams", ["uid", "==", user.uid]);
   const { documents: LicenseTeams } = useTeamLicenseCollection("Teams");
 
   useEffect(() => {
+    const combinedOptions = [];
+
     if (Logo?.length > 0) {
       const TeamOption = Logo?.map((logo) => ({
         value: logo.img,
         label: logo.firstName + "." + logo.secondName,
       }));
-      setTeamOption((prev) => [prev, ...TeamOption]);
+      combinedOptions.push(...TeamOption);
     }
     if (LicenseTeams?.length > 0) {
       const LicenseTeamOption = LicenseTeams?.map((item) => ({
         value: item.img,
         label: item.firstName + "." + item.secondName,
       }));
-      setTeamOption((prev) => [prev, ...LicenseTeamOption]);
+      combinedOptions.push(...LicenseTeamOption);
     }
+
+    setTeamOption(combinedOptions);
   }, [Logo, LicenseTeams]);
 
   return teamOption;
