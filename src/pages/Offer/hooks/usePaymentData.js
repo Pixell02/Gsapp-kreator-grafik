636bbd25e30226b88309431fc3dadd2ce7ddb016
Pixel <nikodems1308@gmail.com>
2023-11-03 22:09:25 +0100

    make changes with writing default values

--- src/pages/Offer/hooks/usePaymentData.js
+++ src/pages/Offer/hooks/usePaymentData.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import { useAuthContext } from '../../../hooks/useAuthContext'
-import { countries } from '../components/countries'
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { useAuthContext } from '../../../hooks/useAuthContext';
+import { countries } from '../components/countries';
 
 const usePaymentData = (userData) => {
   
-  const { user } = useAuthContext()
-  // Creating payment data state
+  const { user } = useAuthContext();
+  
+  
   const [paymentData, setPaymentData] = useState({
     totalAmount: "",
     extOrderId: Date.now().toString(),
@@ -15,7 +16,7 @@ const usePaymentData = (userData) => {
     buyer: {
       firstName: '',
       lastName: '',
-      email: '',
+      email: user.email,
       delivery: {
         street: '',
         postalCode: '',
@@ -23,76 +24,71 @@ const usePaymentData = (userData) => {
         countryCode: 'PL'
       }
     }
-  })
-
-  
+  });
   useEffect(() => {
-    if (userData) {
-      if (userData.length > 0) {
-        countries.forEach(country => {
-          if (country.label === userData[0].country) {
-            setPaymentData((prev) => ({
-              ...prev,
-              NIP: userData[0].NIP,
-              companyName: userData[0].companyName,
-              buyer: {
-                firstName: userData[0].firstName,
-                lastName: userData[0].lastName,
-                email: user.email,
-                delivery: {
-                  street: userData[0].address,
-                  postalCode: userData[0].postCode,
-                  city: userData[0].city,
-                  countryCode: country.value
-                }
-              }
-
-            }));
+    if (userData && userData.length > 0) {
+      const selectedCountry = countries.find(country => country.label === userData[0].country);
+      if (selectedCountry) {
+        const updatedPaymentData = {
+          NIP: userData[0].NIP,
+          companyName: userData[0].companyName,
+          buyer: {
+            ...paymentData.buyer,
+            firstName: userData[0].firstName,
+            lastName: userData[0].lastName,
+            email: user.email,
+            delivery: {
+              ...paymentData.buyer.delivery,
+              street: userData[0].address,
+              postalCode: userData[0].postCode,
+              city: userData[0].city,
+              countryCode: selectedCountry.value
+            }
           }
-        })
+        };
+        setPaymentData(prev => ({
+          ...prev,
+          ...updatedPaymentData
+        }) );
       }
     }
-  },[userData, user])
+  }, [userData, user]);
 
- // Creating arrow function to hold payment data
   
+
   const handleChange = (e) => {
-    const { name, value} = e.target
+    const { name, value } = e.target;
     setPaymentData(prev => ({
       ...prev,
       [name]: value
     }));
-  }
+  };
 
   const handleDataChange = (e) => {
-  const { name, value } = e.target;
-  setPaymentData(prevPaymentData => ({
-    ...prevPaymentData,
-    buyer: {
-      ...prevPaymentData.buyer,
-      [name]: value
-    }
-  }));
-  };
-  const handleDeliveryDataChange = (e) => { 
     const { name, value } = e.target;
     setPaymentData(prevPaymentData => ({
       ...prevPaymentData,
       buyer: {
         ...prevPaymentData.buyer,
-        delivery: {
-          ...prevPaymentData.buyer.delivery,
-          [name]: value
-        }
+        [name]: value
       }
     }));
-  }
+    };
+    const handleDeliveryDataChange = (e) => { 
+      const { name, value } = e.target;
+      setPaymentData(prevPaymentData => ({
+        ...prevPaymentData,
+        buyer: {
+          ...prevPaymentData.buyer,
+          delivery: {
+            ...prevPaymentData.buyer.delivery,
+            [name]: value
+          }
+        }
+      }));
+    }
 
-  return {paymentData, handleChange, handleDataChange, handleDeliveryDataChange, setPaymentData}
+  return { paymentData, handleChange, handleDataChange, handleDeliveryDataChange, setPaymentData };
+};
 
-}
-
-
-
-export default usePaymentData
-
+export default usePaymentData;
