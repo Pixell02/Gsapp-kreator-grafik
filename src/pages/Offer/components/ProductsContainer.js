4ce6d9bbb8593f1c7576ffb46635adf0b52d535b
Pixel <nikodems1308@gmail.com>
2023-11-03 22:06:58 +0100

    reduce code, add new array

--- src/pages/Offer/components/ProductsContainer.js
+++ src/pages/Offer/components/ProductsContainer.js
@@ -5,123 +5,50 @@ import { useEffect } from "react";
 import { useContext } from "react";
 import { LanguageContext } from "../../../context/LanguageContext";
 import translate from "../locales/translate.json";
+import usePromoCodeContext from "../hooks/usePromoCodeContext";
 
 export default function ProductsContainer(props) {
+  const { language } = useContext(LanguageContext);
 
-  const {language} = useContext(LanguageContext)
-  
-  const services = [
-  { id: 3, name: translate.matchDay[language], price: "7000" },
-  { id: 4, name: translate.eventAnnouncement[language], price: "7000" },
-  { id: 5, name: translate.gool[language], price: "7000" },
-  { id: 6, name: translate.startingXI[language], price: "7000" },
-  { id: 7, name: translate.result[language], price: "7000" },
-  { id: 8, name: translate.crest[language], price: "5000" },
-  { id: 9, name: translate.players[language], price: "15000" },
+  const licenseRadioArray = [
+    { value: "2800", name: "Licencja MAX 1 miesiąc", title: translate.month[language] },
+    { value: "8148", name: "Licencja MAX 3 miesięce", title: translate.month3[language] },
+    { value: "15792", name: "Licencja MAX 6 miesięce", title: translate.month6[language] },
+    { value: "0", name: "other", title: translate.services[language] }
   ];
-  
-  
-  useEffect(() => {
-    
-    if (props.promoCode.products >= 3) {
-      const updatedProducts = props.products.map((product) => {
-        const matchingService = services.find((item) => item.id === props.promoCode.products && item.name === product.name);
-        if (matchingService) {
-          return {
-            ...product,
-            unitPrice: matchingService.price * (1 - props.promoCode.percentage / 100)
-          };
-        }
-        return product;
-      });
-      if (JSON.stringify(updatedProducts) !== JSON.stringify(props.products)) {
-        props.setProducts(updatedProducts);
-      }
-    } else {
-      if (props.promoCode.products === 2 && props.radioType === "other") {
-        const updatedProducts = props.products.map((product) => {
-          const matchingService = services.find((service) => service.name === product.name);
-          if (matchingService) {
-            return {
-              ...product,
-              unitPrice: matchingService.price * (1 - props.promoCode.percentage / 100)
-            };
-          }
-          return product;
-        });
-        if (JSON.stringify(updatedProducts) !== JSON.stringify(props.products)) {
-          props.setProducts(updatedProducts);
-        }
-      }
-    }
-  }, [props.promoCode.code, props.radioType, props.products, props.setProducts]);
-  
-   // Zależności dla hooka useEffect
-  
-  useEffect(() => {
-    if (props.promoCode.products === 1 && props.radioType === "Licencja MAX 1 miesiąc") {
-      const updatedProducts = [...props.products];
-      const product = updatedProducts[0];
-      if (product?.name === "Licencja MAX 1 miesiąc") { // Zastąp "desired_product_name" nazwą pożądanego produktu
-        product.unitPrice = 5000 * (1 - props.promoCode.percentage / 100);
-      }
-      props.setProducts(updatedProducts);
-    }
-  },[props.promoCode, props.radioType])
 
   return (
     <>
       <div className="logo-block">
-        <img src={logo} className="logo-image" />
+        <img src={logo} className="logo-image" alt="logo" />
       </div>
       <div className="price-container">
-        <h1>{props.paymentData.totalAmount / 100}zł</h1>
+        <h1>{(props.paymentData.totalAmount / 100).toFixed(2)}zł</h1>
       </div>
       <div className="package-container">
-        <label>
-          <input
-            type="radio"
-            value="5000"
-            name="Licencja MAX 1 miesiąc"
-            onChange={(e) => {
-              props.handleCheckboxChange(e);
-              props.setRadioType(e.target.name);
-            }}
-            checked={props.radioType === "Licencja MAX 1 miesiąc"}
-            defaultChecked
-          />
-          <span>{translate.month[language]}</span>
-        </label>
-
-        <label>
-          <input
-            type="radio"
-            value="0"
-            name="other"
-            onChange={(e) => {
-              props.handleCheckboxChange(e);
-              props.setUnitPrice(e.target.value);
-              props.setRadioType(e.target.name);
-            }}
-            checked={props.radioType === "other"}
-          />
-          <span>{translate.services[language]}</span>
-        </label>
+        {licenseRadioArray.map((item) => (
+          <label>
+            <input
+              type="radio"
+              value={item.value}
+              name={item.name}
+              onChange={(e) => {
+                props.handleCheckboxChange(e);
+                props.setRadioType(e.target.name);
+              }}
+              checked={props.radioType === item.name}
+              defaultChecked
+            />
+            <span>{item.title}</span>
+          </label>
+        ))}
       </div>
-      <label>
-        <input type="radio" />
-      </label>
       {props.radioType && props.radioType === "other" && (
         <div className="mt-3">
           <a href="http://grafika-sportowa.pl/sklep/">Zakup usług dostępne pod linkiem</a>
         </div>
       )}
-      <CodeInput
-        usedCode={props.usedCode}
-        setUsedCode={props.setUsedCode}
-        handleUseCode={props.handleUseCode}
-        alert={props.alert}
-      />
+      <CodeInput />
     </>
   );
 }
