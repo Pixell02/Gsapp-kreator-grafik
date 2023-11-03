2ae6167340f478e3b6b2765e4f07653d6282d2e0
Pixel <nikodems1308@gmail.com>
2023-11-03 22:17:15 +0100

    nothing

--- src/pages/posterCreator/components/hooks/useUniqueKey.js
+++ src/pages/posterCreator/components/hooks/useUniqueKey.js
@@ -1,6 +1,7 @@
 const useUniqueKey = (fabricRef) => {
   const getUniqueTextArray = (array) => {
     const uniqueClasses = new Set();
+    
     array.reverse();
     return array.filter((item) => {
       if (!uniqueClasses.has(item.className) && fabricRef.current?._objects?.some((obj) => obj.className === item.className)) {
