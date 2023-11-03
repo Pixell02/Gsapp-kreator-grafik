cfd20aad18705d1b7588a07bd001b8f12172b8e0
Pixel <nikodems1308@gmail.com>
2023-11-03 22:04:51 +0100

    add if

--- src/pages/Creator/components2/EditPanelComponent/SingleElements/functions/themeOption.js
+++ src/pages/Creator/components2/EditPanelComponent/SingleElements/functions/themeOption.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 
 const findThemeOption = (coords, themeOption, text) => {
-  
-  coords.themeOption.forEach((theme, i) => {
-    if ((theme.color === themeOption.label) || (theme.label === themeOption.label)) {
+  if (themeOption) {
+    coords.themeOption.forEach((theme, i) => {
+      if ((theme.color === themeOption.label) || (theme.label === themeOption.label)) {
       
-      text.set({
-        fill: theme.Fill
-      })
-    }
-  })
-  
+        text.set({
+          fill: theme.Fill
+        })
+      }
+    })
+  }
 }
 
 
