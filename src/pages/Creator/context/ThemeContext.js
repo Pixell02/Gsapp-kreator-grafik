eb423427a22aef21430fd82881bcac977cbd5fec
Pixel <nikodems1308@gmail.com>
2023-11-06 16:40:13 +0100

    create context file for theme color

---
+++ src/pages/Creator/context/ThemeContext.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+
+
+export const ThemeContext = createContext();
+
+
+export const ThemeProvider = ({ children }) => {
+
+  const [themeColor, setThemeColor] = useState(null)
+
+  return <ThemeContext.Provider value={{themeColor, setThemeColor}}>
+    {children}
+  </ThemeContext.Provider>;
+}
