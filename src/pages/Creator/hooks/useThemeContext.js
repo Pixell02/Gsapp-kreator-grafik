53dc6ee3633f88dce934a4a4c8a43fe8d2d5a9f8
Pixel <nikodems1308@gmail.com>
2023-11-06 16:37:35 +0100

    create custom hook for context

---
+++ src/pages/Creator/hooks/useThemeContext.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext';
+
+const useThemeContext = () => {
+
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw Error('useThemeContext must be used within a ThemeProvider');
+  }
+
+  return context;
+}
+
+export default useThemeContext
