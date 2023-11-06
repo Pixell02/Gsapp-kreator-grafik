2a2272fee3570f422a2bd523fa10ae6f7f397948
Pixel <nikodems1308@gmail.com>
2023-11-06 16:36:30 +0100

    add provider

--- src/pages/Creator/Creator.js
+++ src/pages/Creator/Creator.js
@@ -1,16 +1,19 @@
 import LeftBar from '../../components/Left-Bar';
 import WorkSpace from './WorkSpace';
 import { ImageRefProvider } from './context/ImageRefContext';
+import { ThemeProvider } from './context/ThemeContext';
 function Creator() {
 
   return (
-      <ImageRefProvider>
+    <ImageRefProvider>
+      <ThemeProvider>
         <div className="page-container">
           <div className='content-wrap'>
             <LeftBar />
             <WorkSpace />
           </div>
       </div>
+      </ThemeProvider>
     </ImageRefProvider>
     )
 }
