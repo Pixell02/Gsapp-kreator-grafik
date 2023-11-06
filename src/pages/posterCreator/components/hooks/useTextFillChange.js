b8b2757e46f269ed82da53fa87d07cbdd63d88f5
Pixel <nikodems1308@gmail.com>
2023-11-06 16:44:09 +0100

    add useEffect to search className, connect className with Fill, add second useEffect to set fill of text

--- src/pages/posterCreator/components/hooks/useTextFillChange.js
+++ src/pages/posterCreator/components/hooks/useTextFillChange.js
@@ -1,26 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useBackgroundContext from "./useBackgroundContext";
 import useGlobalPropertiesContext from "./useGlobalPropertiesContext";
 
-const useTextFillChange = (fabricRef, coords) => {
+const useTextFillChange = (fabricRef) => {
   const { color } = useBackgroundContext();
   const { globalProperties } = useGlobalPropertiesContext();
-  const themeOptions = globalProperties?.[coords.className]?.themeOption;
-  const themeOption = themeOptions?.find((option) => option.label === color.name);
-
-  const fill = themeOption ? themeOption.Fill : coords.Fill;
+  const [keysAndFill, setKeysAndFill] = useState({});
+  const fill = "asdasdasd";
 
   useEffect(() => {
-    if (fabricRef.current?.backgroundImage) {
-      const canvas = fabricRef.current;
-
-      const activeObject = canvas.getActiveObject();
-      if (activeObject) {
-        activeObject.set("fill", fill);
-        canvas.renderAll();
+    if (fabricRef.current?._objects) {
+      const objectFill = {};
+      const keys = Object.keys(globalProperties);
+      for (const key of keys) {
+        if (Array.isArray(globalProperties[key])) {
+          globalProperties[key].forEach((item) => {
+            const fill = item.themeOption.find((option) => option.label === color);
+            if (fill) objectFill[item.className] = fill.Fill;
+          });
+        } else {
+          const fill = globalProperties?.[key]?.themeOption?.find((option) => option.label === color);
+          if (fill) objectFill[globalProperties[key].className] = fill.Fill;
+        }
       }
+      setKeysAndFill(objectFill);
     }
-  }, [fill, color, fabricRef]);
+  }, [color, globalProperties, fabricRef]);
+
+  useEffect(() => {
+    const canvas = fabricRef.current;
+    if (!canvas?._objects) return;
+    const objects = canvas.getObjects();
+    if (!objects) return;
+    const keys = Object.keys(keysAndFill);
+    for (const key of keys) {
+      objects.forEach((item) => {
+        console.log(item.className, keysAndFill[key])
+        if (item?.className === key && keysAndFill[key]) {
+          
+          item.set("fill", keysAndFill[key]);
+        }
+      });
+    }
+    fabricRef.current.renderAll();
+  }, [keysAndFill, fabricRef]);
 
   return fill;
 };
