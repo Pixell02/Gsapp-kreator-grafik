cbcf62639e938af631eecb4c96ac61e767c4c463
Zibi95 <sylwester.sob95@gmail.com>
2023-12-14 18:14:48 +0100

    add typescript to project

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AuthContextProvider } from './context/AuthContext';
+import App from './App';
+import { LicenseProvider } from './context/LicenseContext';
+import { LanguageProvider } from './context/LanguageContext';
+
+ReactDOM.render(
+  <AuthContextProvider>
+    <LicenseProvider>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </LicenseProvider>
+  </AuthContextProvider>,
+  document.getElementById('root')
+);
