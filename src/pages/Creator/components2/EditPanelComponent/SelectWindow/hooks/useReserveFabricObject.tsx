3d65d5b981397e907e826e0e5be59c167a9c3b05
Pixel <nikodems1308@gmail.com>
2024-01-19 23:37:40 +0100

    format textbox object for reserve players

---
+++ src/pages/Creator/components2/EditPanelComponent/SelectWindow/hooks/useReserveFabricObject.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { useTeamContext } from "../../../../context/teamContext";
+import useTextboxLayer from "../../../../hooks/useTextboxLayer";
+import { DocumentData } from "firebase/firestore";
+
+type Player = {
+  format: string;
+  number?: string;
+  firstName: string;
+  secondName: string;
+  age: number;
+};
+
+const useReserveFabricObject = (fabricRef: React.MutableRefObject<fabric.Canvas>, coords: DocumentData) => {
+  const { setTextValue } = useTextboxLayer(coords, fabricRef);
+  const { selectedReserve, selectedPreset } = useTeamContext();
+  useEffect(() => {
+    const handleReserveFormat = (selectedReserve: Player[]) => {
+      const currentDate = new Date();
+      const currentYear = currentDate.getFullYear();
+      let text: string = "";
+      if (!selectedReserve || selectedReserve.length === 0) return;
+      selectedReserve.forEach((reserved) => {
+        let reserve = "";
+        if (coords.format === "NumDotSurName" || coords.Format === "NumDotSurName") {
+          reserve = (reserved.number || "") + "." + reserved.secondName;
+        } else if (coords.format === "NumSurName" || coords.Format === "NumSurName") {
+          reserve = (reserved.number || "") + "." + reserved.secondName;
+        } else if (coords.format === "dotted" || coords.Format === "dotted") {
+          reserve = (reserved.number || "") + "." + reserved.firstName[0] + "." + reserved.secondName;
+        } else if (coords.format === "oneDot" || coords.Format === "oneDot") {
+          reserve = (reserved.number || "") + "." + reserved.firstName[0] + "." + reserved.secondName;
+        } else {
+          reserve = reserved.secondName;
+        }
+        if (currentYear - Number(reserved.age) <= 21) {
+          reserve += "(m)";
+        }
+        text = text + " " + reserve + `${coords.Formatter || ","} `;
+      });
+      setTextValue(text);
+    };
+    handleReserveFormat(selectedReserve?.length ? selectedReserve : selectedPreset?.reservePlayers);
+  }, [selectedReserve, selectedPreset]);
+
+  return { selectedReserve, setTextValue };
+};
+
+export default useReserveFabricObject;
