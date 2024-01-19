9d69471c655a0e64dd570b7ebe192d4711e5e4cf
Pixel <nikodems1308@gmail.com>
2024-01-19 23:22:53 +0100

    add types

--- src/pages/Creator/context/CalendarContext.tsx
+++ src/pages/Creator/context/CalendarContext.tsx
@@ -1,33 +1,98 @@
-import { PropsWithChildren, createContext, useContext, useState, useEffect } from "react";
+import { PropsWithChildren, createContext, useContext, useState, useEffect, SetStateAction, Dispatch } from "react";
 import { DocumentData, doc, getDoc } from "firebase/firestore";
 import { db } from "../../../firebase/config";
 import { useParams } from "react-router";
+import { useDoc } from "../../../hooks/useDoc";
 
-const CalendarContext = createContext<null | { graphicProperties: DocumentData | null }>(null);
+export type selectedTeam = {
+  firstName: string;
+  img: string;
+  secondName: string;
+  team: string;
+  uid: string;
+};
 
-export const CalendarContextProvider = ({ children }: PropsWithChildren) => {
+export type Opponent = {
+  firstName: string;
+  img: string;
+  secondName: string;
+  team: string;
+  uid: string;
+};
+
+export type Player = {
+  firstName: string;
+  secondName: string;
+  number?: string;
+  team: string;
+  age?: number;
+  img: string;
+};
+
+type textProps = {
+  [key: string]: string;
+  className: string;
+  value: string;
+};
+
+type CalendarData = {
+  player?: Player[];
+  hostResult?: string[];
+  guestResult?: string[];
+  yourResult?: string;
+  opponentResult?: string;
+  typePlace?: string;
+  selectedGuest?: selectedTeam;
+  Textbox?: textProps[];
+  Text?: textProps[];
+  TextOne?: textProps[];
+  radioChecked?: string;
+  selectedTheme?: string;
+  selectedTeam?: selectedTeam;
+  selectedOpponent?: Opponent;
+  selectedPlacePreset?: string;
+  selectedPlayers?: Player[];
+  selectedReserve?: Player[];
+  capitan?: Player;
+  goalKeeper?: Player;
+};
+
+type calendarDataProps = {
+  calendarData: CalendarData | null;
+  setCalendarData: Dispatch<SetStateAction<CalendarData | null>>;
+  calendarPoster: DocumentData | null;
+};
+
+const CalendarContext = createContext<calendarDataProps | null>(null);
+
+export const CalendarProvider = ({ children }: PropsWithChildren) => {
   const { poster } = useParams();
-  const [graphicProperties, setGraphicProperties] = useState<DocumentData | null>(null);
+  const [calendarData, setCalendarData] = useState<CalendarData | null>(null);
+  const { documents: calendarPoster } = useDoc("calendarPoster", ["coords", "==", poster || ""]);
 
   useEffect(() => {
     const handleGetDoc = async () => {
-      const docRef = doc(db, "calendarCoords", poster || "");
+      if (!poster) return;
+      const docRef = doc(db, "calendarCoords", poster);
       const docSnap = await getDoc(docRef);
-      setGraphicProperties(docSnap.data() as DocumentData);
+      setCalendarData(docSnap.data() as CalendarData);
     };
-    if (!poster) return;
     handleGetDoc();
   }, [poster]);
 
-  return <CalendarContext.Provider value={{ graphicProperties }}>{children}</CalendarContext.Provider>;
+  return (
+    <CalendarContext.Provider value={{ calendarData, setCalendarData, calendarPoster }}>
+      {children}
+    </CalendarContext.Provider>
+  );
 };
 
-export const useCalendarCoords = () => {
-  const coords = useContext(CalendarContext);
+export const useCalendarContext = () => {
+  const context = useContext(CalendarContext);
 
-  if (!coords) {
-    throw new Error("Coords not avaible here!");
+  if (!context) {
+    throw new Error("context not avaible here!");
   }
 
-  return coords;
+  return context;
 };
