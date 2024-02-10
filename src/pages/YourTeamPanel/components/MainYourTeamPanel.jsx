6362a2c4035452ba7a1b064fa87aa23e13d8eddf
Pixel <nikodems1308@gmail.com>
2024-02-10 17:38:53 +0100

    reduce code

--- src/pages/YourTeamPanel/components/MainYourTeamPanel.jsx
+++ src/pages/YourTeamPanel/components/MainYourTeamPanel.jsx
@@ -1,32 +1,13 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import ReturnButton from '../../../components/ReturnButton';
-import ItemContainer from '../../../components/main-content-elements/ItemContainer';
-import Title from '../../../components/main-content-elements/Title';
-import { useAuthContext } from '../../../hooks/useAuthContext';
-import { useCollection } from '../../../hooks/useCollection';
-import './MainYourTeamPanel.css';
-import YourTeamContainer from './YourTeamContainer';
-import AddTeamWindow from './addTeamWindow';
-import translate from './locales/yourTeamPanel.json';
-import TrainersContainer from './TrainersContainer';
-import useEditModal from '../../../hooks/useEditModal';
-import EditTrainerWindow from './EditTrainerWindow';
-import AddTrainerWindow from './AddTrainerWindow';
-import { useLanguageContext } from '../../../context/LanguageContext';
+import { useNavigate } from "react-router-dom";
+import ReturnButton from "../../../components/ReturnButton";
+import Title from "../../../components/main-content-elements/Title";
+import "./MainYourTeamPanel.css";
+import YourTeamContainer from "./YourTeamContainer";
+import translate from "./locales/yourTeamPanel.json";
+import TrainersContainer from "./TrainersContainer";
+import { useLanguageContext } from "../../../context/LanguageContext";
 function MainYourTeamPanel() {
-  const { user } = useAuthContext();
   const { language } = useLanguageContext();
-  const [openModal, setOpenModal] = useState(false);
-  const { documents: Team } = useCollection('Teams', ['uid', '==', user.uid]);
-  const [trainerModal, setTrainerModal] = useState(false);
-  const { isEditModal, openEditModal, closeEditModal } = useEditModal();
-  const [data, setData] = useState(null);
-
-  const editClick = item => {
-    setData(item);
-    openEditModal();
-  };
 
   const navigate = useNavigate();
   return (
@@ -40,54 +21,17 @@ function MainYourTeamPanel() {
             </div>
             <div className="empty-container"></div>
             <div className="d-flex guide-btn-container">
-              <button
-                onClick={() => navigate(`/${language}/guide`)}
-                className="btn primary-btn">
+              <button onClick={() => navigate(`/${language}/guide`)} className="btn primary-btn">
                 {translate.guide[language]}
               </button>
             </div>
           </div>
-          <button
-            className="btn primary-btn"
-            onClick={() => setOpenModal(true)}>
-            {translate.addTeam[language]}
-          </button>
-          <ItemContainer>
-            <YourTeamContainer Team={Team} />
-          </ItemContainer>
+          <YourTeamContainer />
           <div className="mt-5">
-            <button
-              className="btn primary-btn"
-              onClick={() => setTrainerModal(true)}>
-              {translate.addTrainer[language]}
-            </button>
-            <ItemContainer>
-              <TrainersContainer editClick={editClick} />
-            </ItemContainer>
+            <TrainersContainer />
           </div>
         </div>
       </div>
-      {trainerModal && (
-        <AddTrainerWindow
-          Team={Team}
-          open={trainerModal}
-          onClose={() => setTrainerModal()}
-        />
-      )}
-      {openModal && (
-        <AddTeamWindow
-          open={openModal}
-          onClose={() => setOpenModal(false)}
-        />
-      )}
-      {data && isEditModal && (
-        <EditTrainerWindow
-          Team={Team}
-          onClose={closeEditModal}
-          data={data}
-          open={isEditModal}
-        />
-      )}
     </>
   );
 }
