721f2736f2cf777f2dc154aa9b8291c3a5dc482d
Pixel <nikodems1308@gmail.com>
2024-02-18 20:20:09 +0100

    add sponsors

--- src/pages/Creator/Canvas.jsx
+++ src/pages/Creator/Canvas.jsx
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
 import useCanvasPropertiesContext from "./hooks/useCanvasPropertiesContext";
 import useFabricCanvas from "./hooks/useFabricCanvas";
+import { useCollection } from "../../hooks/useCollection";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import useSponsors from "./hooks/useSponsors";
 
-function Canvas({ dataURL, fabricRef }) {
+function Canvas({ dataURL, fabricRef, coords }) {
   const { width, setWidth, height, setHeight } = useCanvasPropertiesContext();
   const { initFabric } = useFabricCanvas();
-  // const { documents: sponsors } = useCollection("Sponsors", ["uid", "==", user.uid])
+  const { user } = useAuthContext();
+  const { documents: sponsors } = useCollection("Sponsors", ["uid", "==", user.uid]);
+  const { sponsorsArray, sponsorHeight } = useSponsors(sponsors, coords);
+
   useEffect(() => {
     const img = new Image();
     img.src = dataURL;
@@ -21,16 +27,25 @@ function Canvas({ dataURL, fabricRef }) {
       initFabric(fabricRef, image);
     };
   }, [dataURL, fabricRef]);
-
   return (
-    <>
-      {/* <div className="sponsors-container" style={{height: "150px", width: "250px"}}>
-      {sponsors?.map((image, index) => (
-        <img key={index} src={image.img} alt={`Img ${index + 1}`} />
-      ))}
-    </div> */}
+    <div className="render-container">
+      <div
+        id="sponsors-container"
+        className="sponsors-container"
+        style={{
+          height: `${coords?.Height}px`,
+          width: `${coords?.Width}px`,
+          backgroundColor: `${coords?.Fill}`,
+          top: `${coords?.Top}px`,
+          left: `${coords?.Left}px`,
+        }}
+      >
+        {sponsorsArray?.map((image) => (
+          <img src={image?.img} style={{ height: `${sponsorHeight}px` }} alt="Img ${i + 1}" />
+        ))}
+      </div>
       <canvas id="canvas" className="resposive-canvas" ref={fabricRef} width={width} height={height} />
-    </>
+    </div>
   );
 }
 
