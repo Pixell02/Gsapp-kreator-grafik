03a112841412142a5a894ab5c16352627786f40a
Pixel <nikodems1308@gmail.com>
2024-02-18 20:14:13 +0100

    change destination of type file with type

--- src/components/SlabBlock.tsx
+++ src/components/SlabBlock.tsx
@@ -2,7 +2,7 @@ import React, { Dispatch, SetStateAction, useRef } from "react";
 import "./slabBlock.css";
 import OptionButton from "./OptionButton";
 import { Place } from "../pages/Opponents/components/PlaceContent";
-import { squadPreset } from "../types/playerAndSquadTypes";
+import { SquadPreset } from "../types/teamTypes";
 
 type Props<T> = {
   item: T;
@@ -11,13 +11,13 @@ type Props<T> = {
   setSelectedModal: Dispatch<SetStateAction<number>>;
 };
 
-const SlabBlock = <T extends Place | squadPreset>({ item, type, setData, setSelectedModal }: Props<T>) => {
+const SlabBlock = <T extends Place | SquadPreset>({ item, type, setData, setSelectedModal }: Props<T>) => {
   const hideElement = useRef(null);
 
   return (
     <div ref={hideElement} className="slab-container">
       <div className="name-container">
-        <span>{(item as Place)?.place || (item as squadPreset).presetName}</span>
+        <span>{(item as Place)?.place || (item as SquadPreset).presetName}</span>
       </div>
       <OptionButton
         setData={setData}
