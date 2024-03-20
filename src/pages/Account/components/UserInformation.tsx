4ad6e62c752986a0f9cba6664ad0cb0931260c54
Pixel <nikodems1308@gmail.com>
2024-03-20 12:25:13 +0100

    change reading to read from admin panel too

--- src/pages/Account/components/UserInformation.tsx
+++ src/pages/Account/components/UserInformation.tsx
@@ -1,28 +1,34 @@
-import React from "react";
 import { useAuthContext } from "../../../hooks/useAuthContext";
 import useUserInformation from "../../../hooks/useUserInformation";
 
-export default function UserInformation() {
+type props = {
+  userInfo?: {
+    email: string;
+    uid: string;
+  };
+};
+
+export default function UserInformation({ userInfo }: props) {
   const { user } = useAuthContext();
   const { userCreatedAt } = useUserInformation(user.uid);
   return (
-    <>
+    <div className="d-flex flex-column w-100 m-3">
       <label>Id konta</label>
       <div className="userId account-data-container">
-        <span className="account-data">{user.uid} </span>
+        <span className="account-data">{userInfo?.uid || user.uid} </span>
       </div>
       {user.email && (
         <>
           <label>E-mail</label>
           <div className="userEmail account-data-container">
-            <span className="account-data">{user.email}</span>
+            <span className="account-data">{userInfo?.email || user.email}</span>
           </div>
         </>
       )}
       <label>Data utworzenia konta</label>
       <div className="userId account-data-container">
-        <span className="account-data">{userCreatedAt} </span>
+        <span className="account-data">{userCreatedAt || ""} </span>
       </div>
-    </>
+    </div>
   );
 }
