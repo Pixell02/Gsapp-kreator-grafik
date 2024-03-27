f630a2d722aabddf91959d953c4fb01824f9072d
Pixel <nikodems1308@gmail.com>
2024-03-27 15:52:20 +0100

    make changes in style and build

--- src/components/ReturnButton.jsx
+++ src/components/ReturnButton.jsx
@@ -1,16 +1,19 @@
-import { Link } from 'react-router-dom';
-import translate from './locales/translate.json';
-import './returnButton.css';
-import { useLanguageContext } from '../context/LanguageContext';
+import { Link } from "react-router-dom";
+import translate from "./locales/translate.json";
+import "./returnButton.css";
+import { useLanguageContext } from "../context/LanguageContext";
 
 const ReturnButton = () => {
   const { language } = useLanguageContext();
   return (
-    <div className="return-container">
-      <Link to={`/${language}/yourCatalog`}>
-        {'<'}
+    <div className="return-container w-75">
+      <Link className="d-flex w-100" to={`/${language}/yourCatalog`}>
+        {"<"}
         {translate.return[language]}
       </Link>
+      {/* <div className="d-flex w-75 justify-content-end">
+        <button className="btn">Samouczek</button>
+      </div> */}
     </div>
   );
 };
